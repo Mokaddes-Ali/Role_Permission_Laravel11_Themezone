c06eb9652e36c0903be3c5dd6e48a8b7a6da8209
mokadde <mokaddes.ru2000@gmail.com>
2024-11-09 08:24:29 +0600

    commit

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+// use Spatie\Permission\Models\Permission;
+
+use App\Models\Permission;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class PermissionSeeder extends Seeder
 {
@@ -12,6 +15,19 @@ class PermissionSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $permissions = [
+            'role-list',
+            'role-create',
+            'role-edit',
+            'role-delete',
+            'product-list',
+            'product-create',
+            'product-edit',
+            'product-delete'
+         ];
+
+         foreach ($permissions as $permission) {
+              Permission::create(['name' => $permission]);
     }
+}
 }
