8dbe8e031f75aa5d5c2c5d0a84eb4d2b9648ca97
mokadde <mokaddes.ru2000@gmail.com>
2024-11-09 08:09:27 +0600

    commit

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use Illuminate\Support\Facades\Hash;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Create Roles
+        $superAdminRole = Role::create(['name' => 'super-admin']); //as super-admin
+        $adminRole = Role::create(['name' => 'admin']);
+        $staffRole = Role::create(['name' => 'staff']);
+        $userRole = Role::create(['name' => 'user']);
+    }
+}
